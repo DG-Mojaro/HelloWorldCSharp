a8ef1b8fddd80e00386fd826b5942d3d5c99872f
DG-Mojaro <joshuascoines@gmail.com>
2024-06-09 05:44:03 +0100

    Added a line including arithmetic operators and string concatenation

--- HelloWorldCSharp/Program.cs
+++ HelloWorldCSharp/Program.cs
@@ -9,6 +9,7 @@ namespace HelloWorldCSharp
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is my Hello World project for C#");
             Console.WriteLine("This is a third line to test git commit");
+            Console.WriteLine("The output of the sum 3+7 is: " + (3 + 7));
         }
     }
 }
